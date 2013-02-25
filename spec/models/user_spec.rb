0361472545d0174a980774531740a76a387a6fae
Mani <manikkathir@arrivusystems.com>
2013-02-25 14:45:51 +0530

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,7 @@ describe User do
 	it { should respond_to(:email) }
 	it { should respond_to(:password_digest) }
 	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:remember_token) }
 	it { should respond_to(:authenticate) }
 	it { should be_valid }
 	describe "when name is not present" do
@@ -30,6 +31,10 @@ describe User do
 	describe "when name is too long" do
 		before { @user.name = "a" * 51 }
 		it { should_not be_valid }
+	end
+	describe "remember token" do
+		before { @user.save }
+		its(:remember_token) { should_not be_blank }
 	end
 	describe "when email format is invalid" do
 		it "should be invalid" do
@@ -81,8 +86,8 @@ describe User do
 				specify { user_for_invalid_password.should be_false }
 			end
 			describe "with a paaword that's too short" do
-				 before {@user.password =@user.password_confirmation = "a" * 5}
+				before {@user.password =@user.password_confirmation = "a" * 5}
 				
+			end
 		end
 	end
-end
